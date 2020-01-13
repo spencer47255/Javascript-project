f88e50469c5c40782454cdf193e098206883dbb0
spencer47255 <57740497+spencer47255@users.noreply.github.com>
2020-01-13 13:47:07 -0800

    Added Basic_HTML_2 and Basic_JavaScript_2.js

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_2.js
@@ -0,0 +1,7 @@
+var a="Hello",b=" World"
+function HW() {
+    var sentence= "How are you doing";
+    sentence += " today?";
+    document.getElementById("HW").innerHTML =a+b;
+    document.getElementById("add").innerHTML=sentence;
+}
